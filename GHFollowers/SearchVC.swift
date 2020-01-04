05d0f686658f63293bcd8edd9622f9820bb8d897
OfftheCode <offthecode@gmail.com>
2020-01-04 17:02:34 +0100

    Finish layout for search VC

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -10,9 +10,77 @@ import UIKit
 
 class SearchVC: UIViewController {
     
+    // MARK: - Views
+    
+    let logoIV          = UIImageView()
+    let searchTF        = GFTextField()
+    let searchButton    = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    // MARK: - View Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        configureLogoIV()
+        configureSearchTF()
+        configureSearchButton()
+        addResignKeyboardTapGesture()
+    }
+    
+    private func configureLogoIV() {
+        view.addSubview(logoIV)
+        logoIV.translatesAutoresizingMaskIntoConstraints = false
+        logoIV.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoIV.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoIV.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoIV.heightAnchor.constraint(equalToConstant: 200),
+            logoIV.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    private func configureSearchTF() {
+        view.addSubview(searchTF)
+        searchTF.delegate = self
+        
+        NSLayoutConstraint.activate([
+            searchTF.topAnchor.constraint(equalTo: logoIV.bottomAnchor, constant: 48),
+            searchTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            searchTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            searchTF.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func configureSearchButton() {
+        view.addSubview(searchButton)
+        
+        NSLayoutConstraint.activate([
+            searchButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            searchButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            searchButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func addResignKeyboardTapGesture() {
+        let tapGesutre = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapGesutre)
+    }
+    
+}
+
+extension SearchVC: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("Returned")
+        
+        return true
     }
     
 }
