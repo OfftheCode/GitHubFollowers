f3e0a965567037f43a96c83c66aa36de7fe042b1
OfftheCode <offthecode@gmail.com>
2020-01-04 20:24:27 +0100

    Add GFTitle label

---
+++ GHFollowers/GFTitleLabel.swift
@@ -0,0 +1,37 @@
+//
+//  GFTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Piotr Szadkowski on 04/01/2020.
+//  Copyright © 2020 Piotr Szadkowski. All rights reserved.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.boldSystemFont(ofSize: fontSize)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
