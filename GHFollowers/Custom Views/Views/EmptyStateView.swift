9ffc8c3beadc25411e75c98928524a942d7cfb34
OfftheCode <offthecode@gmail.com>
2020-01-12 22:25:12 +0100

    restructure filetree

---
+++ GHFollowers/Custom Views/Views/EmptyStateView.swift
@@ -0,0 +1,9 @@
+//
+//  EmptyStateView.swift
+//  GHFollowers
+//
+//  Created by Piotr Szadkowski on 12/01/2020.
+//  Copyright © 2020 Piotr Szadkowski. All rights reserved.
+//
+
+import Foundation
