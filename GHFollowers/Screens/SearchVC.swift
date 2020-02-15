0e095b5c1245317a6c40e4aa044332309ec5803e
OfftheCode <offthecode@gmail.com>
2020-02-16 00:02:47 +0100

    Add support for 5s on search screen

--- GHFollowers/Screens/SearchVC.swift
+++ GHFollowers/Screens/SearchVC.swift
@@ -16,18 +16,32 @@ class SearchVC: UIViewController {
     let searchTF        = GFTextField()
     let searchButton    = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
     
+    var logoTopConstraint: NSLayoutConstraint!
+    
     // MARK: - Properties
     
     var isUsernameEntered: Bool {
         return !searchTF.text!.isEmpty
     }
     
-    // MARK: - View Lifecycle
+    // MARK: - Init
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: true)
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboard(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboard(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,13 +52,29 @@ class SearchVC: UIViewController {
         addResignKeyboardTapGesture()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    @objc private func handleKeyboard(_ notification: NSNotification) {
+        guard is4Inch else { return }
+        if notification.name == UIResponder.keyboardWillShowNotification {
+            UIView.animate(withDuration: 0.3) { self.logoTopConstraint.constant = 0; self.view.layoutIfNeeded() }
+        } else {
+            UIView.animate(withDuration: 0.3) { self.logoTopConstraint.constant = 80; self.view.layoutIfNeeded() }
+        }
+    }
+    
     private func configureLogoIV() {
         view.addSubview(logoIV)
         logoIV.translatesAutoresizingMaskIntoConstraints = false
         logoIV.image = UIImage(named: "gh-logo")!
         
+        logoTopConstraint = logoIV.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80)
+        
         NSLayoutConstraint.activate([
-            logoIV.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoTopConstraint,
             logoIV.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoIV.heightAnchor.constraint(equalToConstant: 200),
             logoIV.widthAnchor.constraint(equalToConstant: 200)
